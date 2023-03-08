e326680936149a3f2c57f77c9df8182cbabf2d38
jvnlee <lhjun1028@naver.com>
2023-03-08 12:44:52 +0900

    initial commit

---
+++ src/main/java/com/jvnlee/catchdining/CatchdiningApplication.java
@@ -0,0 +1,13 @@
+package com.jvnlee.catchdining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchdiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchdiningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvnlee/catchdining/CatchdiningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvnlee.catchdining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchdiningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
