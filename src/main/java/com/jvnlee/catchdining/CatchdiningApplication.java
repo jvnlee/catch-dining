2cc94931a5f0fe920c6e199efde2593565de442e
jvnlee <lhjun1028@naver.com>
2023-03-11 22:09:16 +0900

    fix: @EnableJpaAuditing 어노테이션 추가
    Auditing 데이터를 사용하기 위해 BaseEntity를 작업해놓고 실수로 Auditing을 켜놓지 않아서 @EnableJpaAuditing 어노테이션을 추가함.

--- src/main/java/com/jvnlee/catchdining/CatchdiningApplication.java
+++ src/main/java/com/jvnlee/catchdining/CatchdiningApplication.java
@@ -2,8 +2,10 @@ package com.jvnlee.catchdining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CatchdiningApplication {
 
 	public static void main(String[] args) {
