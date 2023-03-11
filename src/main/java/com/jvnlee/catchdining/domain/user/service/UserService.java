1b63850494c3f4d9929e81faf6f2feada3a942fc
jvnlee <lhjun1028@naver.com>
2023-03-11 23:51:45 +0900

    feat: UserService에 username과 phoneNumber 중복 검증 로직 추가
    회원 가입 또는 회원 정보 업데이트 시, username과 phoneNumber가 unique 컬럼이므로 중복되지 않게 검증하는 로직을 메서드로 생성함.

--- src/main/java/com/jvnlee/catchdining/domain/user/service/UserService.java
+++ src/main/java/com/jvnlee/catchdining/domain/user/service/UserService.java
@@ -5,6 +5,7 @@ import com.jvnlee.catchdining.domain.user.dto.UserSearchDto;
 import com.jvnlee.catchdining.domain.user.model.User;
 import com.jvnlee.catchdining.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,8 @@ public class UserService {
     private final UserRepository userRepository;
 
     public void join(UserDto userDto) {
+        validateUsername(userDto);
+        validatePhoneNumber(userDto);
         User newUser = new User(userDto);
         userRepository.save(newUser);
     }
@@ -27,12 +30,26 @@ public class UserService {
     }
 
     public void update(Long id, UserDto userDto) {
+        validateUsername(userDto);
+        validatePhoneNumber(userDto);
         User user = userRepository.findById(id).orElseThrow();
         user.update(userDto);
     }
 
     public void delete(Long id) {
         userRepository.deleteById(id);
+    }
+
+    private void validateUsername(UserDto userDto) {
+        if (userRepository.findByUsername(userDto.getUsername()).isPresent()) {
+            throw new DuplicateKeyException("이미 존재하는 username 입니다.");
+        }
+    }
+
+    private void validatePhoneNumber(UserDto userDto) {
+        if (userRepository.findByPhoneNumber(userDto.getPhoneNumber()).isPresent()) {
+            throw new DuplicateKeyException("이미 존재하는 연락처입니다.");
+        }
     }
 
 }
