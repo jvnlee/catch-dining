28d3356099f1785faaba4ff00a695b5b0a283f95
jvnlee <lhjun1028@naver.com>
2023-03-13 16:40:37 +0900

    feat: UserDetailsService를 구현하는 UserLoginService 클래스 추가
    UserDetailsService는 loadUserByUsername()으로 UserDetails를 가져오는 기능을 가짐. User 엔티티에 UserDetails를 구현시켰으므로 username을 가지고 User 엔티티를 조회해서 반환하도록 구현함.
    
    login() 메서드는 DTO에 담겨온 로그인 정보인 username과 password를 추출하고, 해당 User의 authorities 데이터와 함께 인증 토큰을 생성함. 토큰이 AuthenticationManager에 의해 정상 인증되면 Authentication 객체가 생성됨. 해당 객체는 SecurityContextHolder에 담겨 사용자의 인증 상태를 유지시킴.

---
+++ src/main/java/com/jvnlee/catchdining/domain/user/service/UserLoginService.java
@@ -0,0 +1,48 @@
+package com.jvnlee.catchdining.domain.user.service;
+
+import com.jvnlee.catchdining.domain.user.dto.UserLoginDto;
+import com.jvnlee.catchdining.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserLoginService implements UserDetailsService {
+
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    private final UserRepository userRepository;
+
+    public void login(UserLoginDto userLoginDto) {
+        String username = userLoginDto.getUsername();
+        String password = userLoginDto.getPassword();
+        Collection<? extends GrantedAuthority> authorities = userRepository.findByUsername(username).orElseThrow().getAuthorities();
+
+        // 인증 토큰 생성 (username, password, authorities)
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, authorities);
+
+        // AuthenticationManager에게 인증 토큰을 넘겨 인증 진행
+        Authentication auth = authenticationManagerBuilder.getObject().authenticate(authToken);
+
+        // 인증을 마친 Authentication 객체를 SecurityContextHolder에 보관
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow();
+    }
+
+}
