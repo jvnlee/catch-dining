e010f61f9220dfb26bd336fe517af1502a2d97ae
jvnlee <lhjun1028@naver.com>
2023-03-17 00:20:08 +0900

    feat: UserLoginController에 로그인 실패 시 예외를 처리하는 ExceptionHandler 메서드 추가
    UserService의 login 로직이 호출되었을 때 발생 가능한 예외인 DisabledException과 LockedException은 현재 서비스 스펙에 없기 때문에 BadCredentialsException에 대해서만 예외를 처리함.
    
    BadCredentialsException은 credential에 해당하는 User의 password가 올바르게 입력되지 않았을 때 발생할 수 있음. 로그인 시도 시 username이 잘못된 경우에는 UserNotFoundException이 발생하게 되고, 이는 이미 정의해놓은 ControllerAdvice에 의해 처리되기 때문에 따로 해당 컨트롤러에 예외 처리를 하지 않음.

--- src/main/java/com/jvnlee/catchdining/domain/user/controller/UserLoginController.java
+++ src/main/java/com/jvnlee/catchdining/domain/user/controller/UserLoginController.java
@@ -5,6 +5,8 @@ import com.jvnlee.catchdining.domain.user.dto.JwtDto;
 import com.jvnlee.catchdining.domain.user.dto.UserLoginDto;
 import com.jvnlee.catchdining.domain.user.service.UserLoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -23,6 +25,12 @@ public class UserLoginController {
         JwtDto jwtDto = userLoginService.login(userLoginDto);
         response.setHeader(AUTHORIZATION, "Bearer " + jwtDto.getAccessToken() + " " + jwtDto.getRefreshToken());
         return new Response("로그인 성공");
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response handleBadCredentials() {
+        return new Response("비밀번호가 올바르지 않습니다.");
     }
 
 }
