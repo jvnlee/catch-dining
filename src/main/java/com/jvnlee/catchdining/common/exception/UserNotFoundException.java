5ad2aa8e00c620f1857298d1000559270de8fd71
Hyun Jun <76623442+jvnlee@users.noreply.github.com>
2023-03-18 03:50:01 +0900

    Revert "Feature/#5 user api login"

--- src/main/java/com/jvnlee/catchdining/common/exception/UserNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.jvnlee.catchdining.common.exception;
-
-public class UserNotFoundException extends RuntimeException {
-}
--- src/main/java/com/jvnlee/catchdining/domain/user/service/UserService.java
+++ src/main/java/com/jvnlee/catchdining/domain/user/service/UserService.java
@@ -1,13 +1,11 @@
 package com.jvnlee.catchdining.domain.user.service;
 
-import com.jvnlee.catchdining.common.exception.UserNotFoundException;
 import com.jvnlee.catchdining.domain.user.dto.UserDto;
 import com.jvnlee.catchdining.domain.user.dto.UserSearchDto;
 import com.jvnlee.catchdining.domain.user.model.User;
 import com.jvnlee.catchdining.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,32 +18,22 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    private final PasswordEncoder passwordEncoder;
-
     public void join(UserDto userDto) {
         validateUsername(userDto);
         validatePhoneNumber(userDto);
-
-        encodePassword(userDto);
-
         User newUser = new User(userDto);
         userRepository.save(newUser);
     }
 
     @Transactional(readOnly = true)
     public UserSearchDto search(String username) {
-        User user = userRepository
-                .findByUsername(username)
-                .orElseThrow(UserNotFoundException::new);
+        User user = userRepository.findByUsername(username).orElseThrow();
         return new UserSearchDto(user);
     }
 
     public void update(Long id, UserDto userDto) {
         validateUsername(id, userDto);
         validatePhoneNumber(id, userDto);
-
-        encodePassword(userDto);
-
         User user = userRepository.findById(id).orElseThrow();
         user.update(userDto);
     }
@@ -80,11 +68,6 @@ public class UserService {
             if (user.get().getId().equals(id)) return; // 같은 phoneNumber 가진 기존 데이터가 자기 자신인 경우는 패스
             throw new DuplicateKeyException("이미 존재하는 연락처입니다.");
         }
-    }
-
-    private void encodePassword(UserDto userDto) {
-        String encodedPassword = passwordEncoder.encode(userDto.getPassword());
-        userDto.setPassword(encodedPassword);
     }
 
 }
