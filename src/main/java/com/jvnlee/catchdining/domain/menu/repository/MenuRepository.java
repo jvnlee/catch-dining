eff8b165094f2a33b1f566c4c896de25eb04e31a
jvnlee <lhjun1028@naver.com>
2023-04-07 17:59:41 +0900

    feat: MenuRepository에 커스텀 메서드 추가
    메뉴 이름으로 조회하는 findByName()과 특정 식당 id로 해당 식당의 메뉴 전체를 조회하는 findAllByRestaurantId()를 추가함

--- src/main/java/com/jvnlee/catchdining/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/jvnlee/catchdining/domain/menu/repository/MenuRepository.java
@@ -2,6 +2,16 @@ package com.jvnlee.catchdining.domain.menu.repository;
 
 import com.jvnlee.catchdining.domain.menu.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    Optional<Menu> findByName(String name);
+
+    @Query("select m from Menu m where m.restaurant.id = :restaurantId")
+    List<Menu> findAllByRestaurantId(Long restaurantId);
+
 }
