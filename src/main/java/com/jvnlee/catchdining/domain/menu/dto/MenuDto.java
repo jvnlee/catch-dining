ef33756b5ccd4f6b789d2103aad8a5b90fa4d10b
jvnlee <lhjun1028@naver.com>
2023-04-09 16:58:45 +0900

    feat: MenuDto와 MenuViewDto에 생성자 롬복 어노테이션 추가

--- src/main/java/com/jvnlee/catchdining/domain/menu/dto/MenuDto.java
+++ src/main/java/com/jvnlee/catchdining/domain/menu/dto/MenuDto.java
@@ -1,8 +1,10 @@
 package com.jvnlee.catchdining.domain.menu.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class MenuDto {
 
     private String name;
--- src/main/java/com/jvnlee/catchdining/domain/menu/dto/MenuViewDto.java
+++ src/main/java/com/jvnlee/catchdining/domain/menu/dto/MenuViewDto.java
@@ -1,7 +1,11 @@
 package com.jvnlee.catchdining.domain.menu.dto;
 
 import com.jvnlee.catchdining.domain.menu.domain.Menu;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class MenuViewDto {
 
     private Long menuId;
