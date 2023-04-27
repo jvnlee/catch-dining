e0d497857d5572bb6833843590731c03f0cf50e9
jvnlee <lhjun1028@naver.com>
2023-04-27 18:48:40 +0900

    feat: Payment 엔티티가 reserveMenus와 일대다 연관관계를 가지도록 설정
    추후에 Payment를 통해 예약한 메뉴 데이터를 조회하기 위함. cascade 옵션을 ALL로 설정해서 Payment를 영속화할 때 ReserveMenu 엔티티들도 함께 영속화되도록 함.

--- src/main/java/com/jvnlee/catchdining/domain/payment/domain/Payment.java
+++ src/main/java/com/jvnlee/catchdining/domain/payment/domain/Payment.java
@@ -1,11 +1,16 @@
 package com.jvnlee.catchdining.domain.payment.domain;
 
 import com.jvnlee.catchdining.entity.BaseEntity;
+import com.jvnlee.catchdining.entity.ReserveMenu;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.*;
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
@@ -21,13 +26,17 @@ public class Payment extends BaseEntity {
 
     private String tid;
 
+    @OneToMany(mappedBy = "payment", cascade = ALL)
+    private List<ReserveMenu> reserveMenus = new ArrayList<>();
+
     private int totalPrice;
 
     @Enumerated(EnumType.STRING)
     private PaymentType paymentType;
 
-    public Payment(String tid, int totalPrice, PaymentType paymentType) {
+    public Payment(String tid, List<ReserveMenu> reserveMenus, int totalPrice, PaymentType paymentType) {
         this.tid = tid;
+        this.reserveMenus = reserveMenus;
         this.totalPrice = totalPrice;
         this.paymentType = paymentType;
     }
