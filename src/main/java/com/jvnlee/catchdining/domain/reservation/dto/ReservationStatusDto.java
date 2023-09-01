1c3e465bf70bb7bc2018b7046a76157940fae0bf
jvnlee <lhjun1028@naver.com>
2023-09-02 00:57:21 +0900

    feat: ReservationStatusDto에 생성자 관련 롬복 어노테이션 추가

--- src/main/java/com/jvnlee/catchdining/domain/reservation/dto/ReservationStatusDto.java
+++ src/main/java/com/jvnlee/catchdining/domain/reservation/dto/ReservationStatusDto.java
@@ -1,9 +1,13 @@
 package com.jvnlee.catchdining.domain.reservation.dto;
 
 import com.jvnlee.catchdining.domain.reservation.model.ReservationStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationStatusDto {
 
     private ReservationStatus status;
