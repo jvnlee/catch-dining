6e95ee486d6054260c6a752e083c5eca80e22485
jvnlee <lhjun1028@naver.com>
2023-09-01 19:08:05 +0900

    feat: 예약 상태를 변경할 때 사용할 ReservationStatusDto 추가

---
+++ src/main/java/com/jvnlee/catchdining/domain/reservation/dto/ReservationStatusDto.java
@@ -0,0 +1,11 @@
+package com.jvnlee.catchdining.domain.reservation.dto;
+
+import com.jvnlee.catchdining.domain.reservation.model.ReservationStatus;
+import lombok.Data;
+
+@Data
+public class ReservationStatusDto {
+
+    private ReservationStatus status;
+
+}
