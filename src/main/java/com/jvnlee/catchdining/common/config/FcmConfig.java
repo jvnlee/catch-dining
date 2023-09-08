e34d7923799c231079a517538b39ed322f2ef7b4
jvnlee <lhjun1028@naver.com>
2023-09-08 19:52:55 +0900

    feat: FcmConfig 클래스 추가 및 FirebaseMessaging 빈 등록

---
+++ src/main/java/com/jvnlee/catchdining/common/config/FcmConfig.java
@@ -0,0 +1,46 @@
+package com.jvnlee.catchdining.common.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+public class FcmConfig {
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() throws IOException {
+        ClassPathResource resource = new ClassPathResource(
+                "firebase/catchdining-44d95-firebase-adminsdk-jc9ud-f3e511bc28.json"
+        );
+
+        InputStream inputStream = resource.getInputStream();
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+
+        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+            for (FirebaseApp app : firebaseAppList) {
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    firebaseApp = app;
+                }
+            }
+        } else {
+            FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(inputStream))
+                    .build();
+
+            firebaseApp = FirebaseApp.initializeApp(firebaseOptions);
+        }
+
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+
+}
