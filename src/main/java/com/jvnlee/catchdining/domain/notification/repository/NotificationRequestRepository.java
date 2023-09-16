96194d28e85d8f9088f21b19b2d3f329fd8fb9f1
jvnlee <lhjun1028@naver.com>
2023-09-17 03:14:36 +0900

    feat: NotificationRequestRepository에 findAllByCond() 메서드 추가
    Service의 notify()를 실행할 때 사용. DB에 저장되어 있는 NotificationRequest 레코드 중에서 조건에 부합하는 레코드를 찾아 리스트로 반환해줌.

--- src/main/java/com/jvnlee/catchdining/domain/notification/repository/NotificationRequestRepository.java
+++ src/main/java/com/jvnlee/catchdining/domain/notification/repository/NotificationRequestRepository.java
@@ -1,7 +1,20 @@
 package com.jvnlee.catchdining.domain.notification.repository;
 
 import com.jvnlee.catchdining.domain.notification.model.NotificationRequest;
+import com.jvnlee.catchdining.entity.DiningPeriod;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface NotificationRequestRepository extends JpaRepository<NotificationRequest, Long> {
+
+    @Query("select nr from NotificationRequest nr " +
+            "where nr.restaurant.id = :restaurantId " +
+            "and nr.desiredDate = :date " +
+            "and nr.diningPeriod = :diningPeriod " +
+            "and nr.headCount between :minHeadCount and :maxHeadCount")
+    List<NotificationRequest> findAllByCond(Long restaurantId, LocalDate date, DiningPeriod diningPeriod, int minHeadCount, int maxHeadCount);
+
 }
