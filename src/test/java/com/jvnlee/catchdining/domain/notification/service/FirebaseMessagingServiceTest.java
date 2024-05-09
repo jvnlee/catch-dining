327c2093301d64c47a059ebcd66d5132e1c2bf52
jvnlee <lhjun1028@naver.com>
2024-05-10 03:40:34 +0900

    test: FirebaseMessagingService에 대한 통합 테스트 추가
    FirebaseMessagingService의 send()가 실패 시 Retryable 어노테이션에 명시한대로 재시도를 하는지 테스트하기 위함. Retry를 작동시키려면 스프링에 대한 의존이 필요하기 때문에 단위테스트로 진행하지 않고, 통합테스트인 @SpringBootTest로 진행함.

---
+++ src/test/java/com/jvnlee/catchdining/domain/notification/service/FirebaseMessagingServiceTest.java
@@ -0,0 +1,40 @@
+package com.jvnlee.catchdining.domain.notification.service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.jvnlee.catchdining.common.exception.MessageSendingFailureException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.retry.annotation.EnableRetry;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@EnableRetry
+public class FirebaseMessagingServiceTest {
+
+    @MockBean
+    FirebaseMessaging firebaseMessaging;
+
+    @Autowired
+    FirebaseMessagingService firebaseMessagingService;
+
+    @Test
+    @DisplayName("FCM에 알림 전송 요청 실패 후 재시도 (3회)")
+    void send_fail_retry() throws FirebaseMessagingException {
+        when(firebaseMessaging.send(any(Message.class)))
+                .thenThrow(FirebaseMessagingException.class);
+
+        assertThatThrownBy(() -> firebaseMessagingService.send(mock(Message.class)))
+                .isInstanceOf(MessageSendingFailureException.class);
+
+        verify(firebaseMessaging, times(3)).send(any(Message.class));
+    }
+
+}
