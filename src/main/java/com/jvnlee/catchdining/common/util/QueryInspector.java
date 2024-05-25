1cf49d0453d8adc68978358ecc27669040e479b4
jvnlee <lhjun1028@naver.com>
2024-05-25 17:55:34 +0900

    refactor: 사용하지 않는 로깅 어노테이션 제거

--- src/main/java/com/jvnlee/catchdining/common/util/QueryInspector.java
+++ src/main/java/com/jvnlee/catchdining/common/util/QueryInspector.java
@@ -1,13 +1,11 @@
 package com.jvnlee.catchdining.common.util;
 
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import org.hibernate.resource.jdbc.spi.StatementInspector;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
-@Slf4j
 @Profile("dev")
 @Component
 @RequestScope
