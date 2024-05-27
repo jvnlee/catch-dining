f0bef12e0cb9851f05af53ae816e4188fe1adb61
jvnlee <lhjun1028@naver.com>
2024-05-27 16:09:37 +0900

    feat: RestaurantRepository애 정렬 조회 메서드 추가
    기존의 findPageByName()에서 name 파라미터의 이름을 keyword로 바꾸는 것이 검색 기능의 확장성(이름이 아닌 다른 정보로 검색하는 기능으로 확장) 측면에서 합당하다고 보아 변경함.
    
    RestaurantSearchResponseDto의 요구사항에 조회할 필드가 늘어남에 따라 쿼리가 늘어남. 리뷰에 포함된 3가지 별점 데이터의 평균치와 해당 식당에 등록된 리뷰의 총 개수를 조회 시 DB 레벨에서 연산하여 가져옴. 그리고 이 연산에 필요한 Review 테이블과 JOIN 하는 과정이 추가됨. (성능 저하 발생 가능 지점)
    
    동적 쿼리에 대한 대응을 아직 고려하지 않았기 때문에 현재로서는 메서드를 필요에 맞게 여러개로 분리함.요청에 sort 옵션이 없는 경우 findPageByKeyword(), sort 옵션이 있는 경우 각각 findPageByKeywordOrderByRating()과 findPageByKeywordOrderByReviewCount()가 DB 조회에 사용됨.

--- src/main/java/com/jvnlee/catchdining/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/jvnlee/catchdining/domain/restaurant/repository/RestaurantRepository.java
@@ -1,12 +1,11 @@
 package com.jvnlee.catchdining.domain.restaurant.repository;
 
-import com.jvnlee.catchdining.domain.restaurant.dto.RestaurantSearchDto;
+import com.jvnlee.catchdining.domain.restaurant.dto.RestaurantSearchResponseDto;
 import com.jvnlee.catchdining.domain.restaurant.model.Restaurant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -14,8 +13,33 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     Optional<Restaurant> findByName(String name);
 
-    @Query("select new com.jvnlee.catchdining.domain.restaurant.dto.RestaurantSearchDto(r.name, r.address) " +
-            "from Restaurant r where r.name like concat('%', :name, '%')")
-    Page<RestaurantSearchDto> findPageByName(@Param("name") String name, Pageable pageable);
+    @Query(
+            "select r.id as id, r.name as name, r.address as address, (avg(rv.tasteRating) + avg(rv.moodRating) + avg(rv.serviceRating)) / 3 as rating, count(rv.id) as reviewCount " +
+            "from Restaurant r " +
+            "left join r.reviews rv " +
+            "where r.name like concat('%', :keyword, '%') " +
+            "group by r.id"
+    )
+    Page<RestaurantSearchResponseDto> findPageByKeyword(String keyword, Pageable pageable);
+
+    @Query(
+            "select r.id as id, r.name as name, r.address as address, round((avg(rv.tasteRating) + avg(rv.moodRating) + avg(rv.serviceRating)) / 3, 2) as rating, count(rv.id) as reviewCount " +
+            "from Restaurant r " +
+            "left join r.reviews rv " +
+            "where r.name like concat('%', :keyword, '%') " +
+            "group by r.id " +
+            "order by (avg(rv.tasteRating) + avg(rv.moodRating) + avg(rv.serviceRating)) / 3 desc"
+    )
+    Page<RestaurantSearchResponseDto> findPageByKeywordOrderByRating(String keyword, Pageable pageable);
+
+    @Query(
+            "select r.id as id, r.name as name, r.address as address, round((avg(rv.tasteRating) + avg(rv.moodRating) + avg(rv.serviceRating)) / 3, 2) as rating, count(rv.id) as reviewCount " +
+            "from Restaurant r " +
+            "left join r.reviews rv " +
+            "where r.name like concat('%', :keyword, '%') " +
+            "group by r.id " +
+            "order by count(rv.id) desc"
+    )
+    Page<RestaurantSearchResponseDto> findPageByKeywordOrderByReviewCount(String keyword, Pageable pageable);
 
 }
