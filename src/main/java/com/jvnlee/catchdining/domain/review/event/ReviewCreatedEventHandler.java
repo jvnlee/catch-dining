bfb05b56f8b83d75f548045fa95fcca9224f3139
jvnlee <lhjun1028@naver.com>
2024-06-10 16:08:23 +0900

    feat: ReviewCreatedEventHandler가 비동기적으로 동작하도록 변경
    동기적인 동작 시, 요청 쓰레드와 동일한 쓰레드로 이벤트를 처리하기 때문에 전체 요청 처리 시간이 증가함. 이벤트 처리(집계 데이터 업데이트)는 요청의 의도인 리뷰 생성에 대해 응답하는 것에는 영향을 주지 않으므로 비동기로 처리해서 전체 처리 시간을 단축함.

--- src/main/java/com/jvnlee/catchdining/domain/review/event/ReviewCreatedEventHandler.java
+++ src/main/java/com/jvnlee/catchdining/domain/review/event/ReviewCreatedEventHandler.java
@@ -2,6 +2,7 @@ package com.jvnlee.catchdining.domain.review.event;
 
 import com.jvnlee.catchdining.domain.restaurant.service.RestaurantReviewStatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -11,6 +12,7 @@ public class ReviewCreatedEventHandler {
 
     private final RestaurantReviewStatService restaurantReviewStatService;
 
+    @Async
     @TransactionalEventListener
     public void handle(ReviewCreatedEvent event) {
         restaurantReviewStatService.update(event.getReviewId());
