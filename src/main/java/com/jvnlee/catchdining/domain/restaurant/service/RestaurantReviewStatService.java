8dd9e07c88a80ac52a3fea5660626feeb650ad4d
jvnlee <lhjun1028@naver.com>
2024-06-10 15:59:14 +0900

    feat: RestaurantReviewStatService 클래스 추가
    update()는 리뷰 ID를 가지고 리뷰의 rating 데이터를 추출해서 RestaurantReviewStat을 업데이트 시키는 역할. REQUIRES_NEW 옵션을 사용하여 기존 트랜잭션인 ReviewService의 트랜잭션에 영향을 주지 않는 새 트랜잭션을 사용하도록 함.
    
    업데이트 타겟인 RestaurantReviewStat의 조회에 실패하는 경우, 새 RestaurantReviewStat 객체를 생성하여 사용하도록 함. 집계 데이터 저장이 한번도 이루어지지 않은 경우, 즉 첫번째 리뷰가 등록되는 경우에 해당함.

---
+++ src/main/java/com/jvnlee/catchdining/domain/restaurant/service/RestaurantReviewStatService.java
@@ -0,0 +1,37 @@
+package com.jvnlee.catchdining.domain.restaurant.service;
+
+import com.jvnlee.catchdining.domain.restaurant.model.RestaurantReviewStat;
+import com.jvnlee.catchdining.domain.restaurant.repository.RestaurantReviewStatRepository;
+import com.jvnlee.catchdining.domain.review.model.Review;
+import com.jvnlee.catchdining.domain.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantReviewStatService {
+
+    private final RestaurantReviewStatRepository restaurantReviewStatRepository;
+
+    private final ReviewRepository reviewRepository;
+
+    @Transactional(propagation = REQUIRES_NEW)
+    public void update(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId).orElseThrow(NoSuchElementException::new);
+        double tasteRating = review.getTasteRating();
+        double moodRating = review.getMoodRating();
+        double serviceRating = review.getServiceRating();
+
+        RestaurantReviewStat restaurantReviewStat = restaurantReviewStatRepository
+                .findById(review.getRestaurant().getId())
+                .orElseGet(() -> RestaurantReviewStat.from(review.getRestaurant()));
+
+        restaurantReviewStat.update(tasteRating, moodRating, serviceRating);
+    }
+
+}
