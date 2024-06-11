c8fc0655171930eca12c9cf1c4376a7f72f69b07
jvnlee <lhjun1028@naver.com>
2024-06-11 22:19:10 +0900

    refactor: SortByConverter 제거
    RestaurantController에서 SortBy enum의 값을 그대로 활용하는 것으로 변경하면서 converter의 필요성이 사라짐

--- src/main/java/com/jvnlee/catchdining/common/config/WebMvcConfig.java
+++ src/main/java/com/jvnlee/catchdining/common/config/WebMvcConfig.java
@@ -1,11 +1,9 @@
 package com.jvnlee.catchdining.common.config;
 
 import com.jvnlee.catchdining.common.interceptor.DataSourceRouterCleanerInterceptor;
-import com.jvnlee.catchdining.common.web.SortByConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,14 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final SortByConverter sortByConverter;
-
     private final DataSourceRouterCleanerInterceptor dataSourceRouterCleanerInterceptor;
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(sortByConverter);
-    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
