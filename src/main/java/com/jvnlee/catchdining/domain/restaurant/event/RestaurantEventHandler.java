744341292afafcc12446fc38338814d915a4450c
jvnlee <lhjun1028@naver.com>
2024-07-26 03:42:02 +0900

    feat: RestaurantEventHandler가 RabbitListener를 사용하도록 변경
    식당 관련 이벤트를 담아놓은 restaurantEventQueue로부터 3가지 종류의 서로 다른 이벤트 객체가 들어올 수 있음. 각 핸들러 메서드가 들어온 이벤트의 타입에 맞게 호출되도록 만들기 위해 RabbitHandler를 사용함.

--- src/main/java/com/jvnlee/catchdining/domain/restaurant/event/RestaurantEventHandler.java
+++ src/main/java/com/jvnlee/catchdining/domain/restaurant/event/RestaurantEventHandler.java
@@ -2,30 +2,32 @@ package com.jvnlee.catchdining.domain.restaurant.event;
 
 import com.jvnlee.catchdining.domain.restaurant.service.RestaurantReviewStatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@RabbitListener(queues = "restaurantEventQueue")
 public class RestaurantEventHandler {
 
     private final RestaurantReviewStatService restaurantReviewStatService;
 
     @Async
-    @TransactionalEventListener
+    @RabbitHandler
     public void handleCreated(RestaurantCreatedEvent event) {
         restaurantReviewStatService.register(event.getRestaurantId(), event.getRestaurantDto());
     }
 
     @Async
-    @TransactionalEventListener
+    @RabbitHandler
     public void handleUpdated(RestaurantUpdatedEvent event) {
         restaurantReviewStatService.update(event.getRestaurantId(), event.getRestaurantDto());
     }
 
     @Async
-    @TransactionalEventListener
+    @RabbitHandler
     public void handleDeleted(RestaurantDeletedEvent event) {
         restaurantReviewStatService.delete(event.getRestaurantId());
     }
