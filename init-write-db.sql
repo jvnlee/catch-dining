fcec454a9ef84a83b2f351c1307a3bb6f320cc41
jvnlee <lhjun1028@naver.com>
2024-07-26 05:35:08 +0900

    fix: write-db 스키마에서 restaurant_review_stat 테이블 제거
    RabbitMQ 테스트 도중 원래 write-db에 없어야 할 테이블 스키마를 발견하여 수정함. restaurant_review_stat 테이블은 read-db에만 존재해야함.

--- init-write-db.sql
+++ init-write-db.sql
@@ -21,27 +21,6 @@ CREATE TABLE IF NOT EXISTS restaurant
     serving_type       VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS restaurant_review_stat
-(
-    restaurant_review_stat_id BIGINT PRIMARY KEY,
-    created_date              DATETIME(6) NOT NULL,
-    last_modified_date        DATETIME(6) NOT NULL,
-    name                      VARCHAR(255) UNIQUE,
-    city                      VARCHAR(255),
-    detail                    VARCHAR(255),
-    district                  VARCHAR(255),
-    province                  VARCHAR(255),
-    street                    VARCHAR(255),
-    phone_number              VARCHAR(20),
-    description               VARCHAR(255),
-    avg_rating                DOUBLE,
-    review_count              INT,
-    country_type              VARCHAR(255),
-    food_type                 VARCHAR(255),
-    serving_type              VARCHAR(255),
-    version                   INT
-);
-
 CREATE TABLE IF NOT EXISTS `user`
 (
     user_id            BIGINT AUTO_INCREMENT PRIMARY KEY,
