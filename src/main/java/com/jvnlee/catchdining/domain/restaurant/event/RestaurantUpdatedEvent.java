a68c6e736b52d6ff6e2a9151867dcb1f3650db00
jvnlee <lhjun1028@naver.com>
2024-07-26 05:34:52 +0900

    feat: 이벤트 클래스에 NoArgsConstructor 추가
    Jackson2JsonMessageConverter가 역직렬화 과정에서 타겟 객체의 기본 생성자를 가지고 인스턴스를 만들어야해서 추가함.

--- src/main/java/com/jvnlee/catchdining/domain/restaurant/event/RestaurantUpdatedEvent.java
+++ src/main/java/com/jvnlee/catchdining/domain/restaurant/event/RestaurantUpdatedEvent.java
@@ -3,8 +3,10 @@ package com.jvnlee.catchdining.domain.restaurant.event;
 import com.jvnlee.catchdining.domain.restaurant.dto.RestaurantDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class RestaurantUpdatedEvent {
 
