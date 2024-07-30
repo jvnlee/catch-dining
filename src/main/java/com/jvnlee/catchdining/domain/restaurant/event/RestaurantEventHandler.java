dadd178b80bee311535bd1dc152faae5ec9be907
jvnlee <lhjun1028@naver.com>
2024-07-31 05:17:46 +0900

    feat: 메시지 처리 후 수동으로 ack 신호를 보내도록 변경
    정상적으로 메시지를 처리한 경우 acknowledgment 신호를, 메시지 처리에 실패한 경우 negative-acknowledgment 신호를 보내게 함. 이를 통해 RabbitMQ는 ack 신호가 돌아온 경우에만 큐에서 메시지를 제거할 수 있음. n-ack 신호가 돌아온 경우에는 메시지를 다시 큐에 넣는 requeue를 수행함.

--- src/main/java/com/jvnlee/catchdining/domain/restaurant/event/RestaurantEventHandler.java
+++ src/main/java/com/jvnlee/catchdining/domain/restaurant/event/RestaurantEventHandler.java
@@ -1,13 +1,21 @@
 package com.jvnlee.catchdining.domain.restaurant.event;
 
 import com.jvnlee.catchdining.domain.restaurant.service.RestaurantReviewStatService;
+import com.rabbitmq.client.Channel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 import static com.jvnlee.catchdining.common.config.RabbitMQConfig.RESTAURANT_EVENT_QUEUE;
+import static org.springframework.amqp.support.AmqpHeaders.DELIVERY_TAG;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @RabbitListener(queues = RESTAURANT_EVENT_QUEUE)
@@ -17,20 +25,45 @@ public class RestaurantEventHandler {
 
     @Async
     @RabbitHandler
-    public void handleCreated(RestaurantCreatedEvent event) {
-        restaurantReviewStatService.register(event.getRestaurantId(), event.getRestaurantDto());
+    public void handleCreated(RestaurantCreatedEvent event, Channel channel, @Header(DELIVERY_TAG) long tag) throws IOException {
+        try {
+            restaurantReviewStatService.register(event.getRestaurantId(), event.getRestaurantDto());
+
+            channel.basicAck(tag, false);
+            log.info("이벤트 처리 완료: {}", event);
+        } catch (IOException e) {
+            channel.basicNack(tag, false, true);
+            log.error("이벤트 처리 실패: {}", event);
+        }
     }
 
     @Async
     @RabbitHandler
-    public void handleUpdated(RestaurantUpdatedEvent event) {
-        restaurantReviewStatService.update(event.getRestaurantId(), event.getRestaurantDto());
+    public void handleUpdated(RestaurantUpdatedEvent event, Channel channel, @Header(DELIVERY_TAG) long tag) throws IOException {
+        try {
+            restaurantReviewStatService.update(event.getRestaurantId(), event.getRestaurantDto());
+
+            channel.basicAck(tag, false);
+            log.info("이벤트 처리 완료: {}", event);
+        } catch (IOException e) {
+            channel.basicNack(tag, false, true);
+            log.error("이벤트 처리 실패: {}", event);
+        }
+
     }
 
     @Async
     @RabbitHandler
-    public void handleDeleted(RestaurantDeletedEvent event) {
-        restaurantReviewStatService.delete(event.getRestaurantId());
+    public void handleDeleted(RestaurantDeletedEvent event, Channel channel, @Header(DELIVERY_TAG) long tag) throws IOException {
+        try {
+            restaurantReviewStatService.delete(event.getRestaurantId());
+
+            channel.basicAck(tag, false);
+            log.info("이벤트 처리 완료: {}", event);
+        } catch (IOException e) {
+            channel.basicNack(tag, false, true);
+            log.error("이벤트 처리 실패: {}", event);
+        }
     }
 
 }
