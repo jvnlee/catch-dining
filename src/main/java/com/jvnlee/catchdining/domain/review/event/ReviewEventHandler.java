52621adaaee613ec5933667af8bc1b92f27a3845
jvnlee <lhjun1028@naver.com>
2024-07-31 15:53:42 +0900

    feat: ReviewEventHandler에도 manual ack 로직 추가

--- src/main/java/com/jvnlee/catchdining/domain/review/event/ReviewEventHandler.java
+++ src/main/java/com/jvnlee/catchdining/domain/review/event/ReviewEventHandler.java
@@ -1,13 +1,20 @@
 package com.jvnlee.catchdining.domain.review.event;
 
 import com.jvnlee.catchdining.domain.restaurant.service.RestaurantReviewStatService;
+import com.rabbitmq.client.Channel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import static com.jvnlee.catchdining.common.config.RabbitMQConfig.REVIEW_EVENT_QUEUE;
+import java.io.IOException;
 
+import static com.jvnlee.catchdining.common.config.RabbitMQConfig.REVIEW_EVENT_QUEUE;
+import static org.springframework.amqp.support.AmqpHeaders.DELIVERY_TAG;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ReviewEventHandler {
@@ -16,13 +23,21 @@ public class ReviewEventHandler {
 
     @Async
     @RabbitListener(queues = REVIEW_EVENT_QUEUE)
-    public void handleCreated(ReviewCreatedEvent event) {
-        restaurantReviewStatService.updateReviewData(
-                event.getRestaurantId(),
-                event.getTasteRating(),
-                event.getMoodRating(),
-                event.getServiceRating()
-        );
+    public void handleCreated(ReviewCreatedEvent event, Channel channel, @Header(DELIVERY_TAG) long tag) throws IOException {
+        try {
+            restaurantReviewStatService.updateReviewData(
+                    event.getRestaurantId(),
+                    event.getTasteRating(),
+                    event.getMoodRating(),
+                    event.getServiceRating()
+            );
+
+            channel.basicAck(tag, false);
+            log.info("이벤트 처리 완료: {}", event);
+        } catch (IOException e) {
+            channel.basicNack(tag, false, true);
+            log.error("이벤트 처리 실패: {}", event);
+        }
     }
 
 }
