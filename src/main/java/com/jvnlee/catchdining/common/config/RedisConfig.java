0cb43670f6945b1f579341dd5d3938297d9cb2f6
jvnlee <lhjun1028@naver.com>
2024-12-16 00:55:06 +0900

    feat: RedisConfig에 RedissonClient Bean 설정 추가

--- src/main/java/com/jvnlee/catchdining/common/config/RedisConfig.java
+++ src/main/java/com/jvnlee/catchdining/common/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.jvnlee.catchdining.common.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +33,14 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port);
+        return Redisson.create(config);
     }
 
 }
