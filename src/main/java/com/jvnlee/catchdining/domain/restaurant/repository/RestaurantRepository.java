a330d7e4ea3c1ef5f7a102f1a4146936afd5bdec
jvnlee <lhjun1028@naver.com>
2025-01-04 03:43:14 +0900

    feat: RestaurantReviewStatRepository를 제거하고 RestaurantRepository로 통일

--- src/main/java/com/jvnlee/catchdining/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/jvnlee/catchdining/domain/restaurant/repository/RestaurantRepository.java
@@ -1,12 +1,25 @@
 package com.jvnlee.catchdining.domain.restaurant.repository;
 
+import com.jvnlee.catchdining.domain.restaurant.dto.RestaurantSearchResultDto;
 import com.jvnlee.catchdining.domain.restaurant.model.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     Optional<Restaurant> findByName(String name);
+
+    @Query("select r from Restaurant r where r.name like concat(:keyword, '%') order by r.avgRating desc")
+    Page<RestaurantSearchResultDto> findPageByKeywordSortByAvgRating(String keyword, Pageable pageable);
+
+    @Query("select r from Restaurant r where r.name like concat(:keyword, '%') order by r.reviewCount desc")
+    Page<RestaurantSearchResultDto> findPageByKeywordSortByReviewCount(String keyword, Pageable pageable);
+
+    @Query("select r from Restaurant r where r.name like concat(:keyword, '%')")
+    Page<RestaurantSearchResultDto> findPageByKeyword(String keyword, Pageable pageable);
 
 }
