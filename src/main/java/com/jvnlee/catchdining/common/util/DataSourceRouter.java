54fcb24bac65936e9e381122e3184e4a55fdd92b
jvnlee <lhjun1028@naver.com>
2025-01-04 03:11:26 +0900

    feat: DataSourceRouter가 현재 트랜잭션의 read only 여부에 따라 라우팅하도록 변경
    기존의 방식은 모든 읽기가 아닌 집계 데이터에 대한 읽기만 READ DB로 라우팅해야 했기 때문에 read only 여부에 따라 라우팅하는 방식을 사용하지 못했음. READ DB의 스키마 자체가 집계 데이터 위주로 저장하는 형태였기 때문. 그러나 이제는 Master-Slave 구조를 사용함에 따라 스키마 차이가 없어졌고, 읽기 트랜잭션은 모두 READ DB로 보낼 수 있게 되었음.

--- src/main/java/com/jvnlee/catchdining/common/util/DataSourceRouter.java
+++ src/main/java/com/jvnlee/catchdining/common/util/DataSourceRouter.java
@@ -1,28 +1,15 @@
 package com.jvnlee.catchdining.common.util;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-@Slf4j
 public class DataSourceRouter extends AbstractRoutingDataSource {
-
-    private final ThreadLocal<DataSourceType> currentDataSource = ThreadLocal.withInitial(() -> DataSourceType.WRITE_DB);
-
-    public void setReadOnly() {
-        currentDataSource.set(DataSourceType.READ_DB);
-    }
-
-    public void reset() {
-        currentDataSource.set(DataSourceType.WRITE_DB);
-    }
 
     @Override
     protected Object determineCurrentLookupKey() {
-        DataSourceType dataSourceType = currentDataSource.get();
-
-        log.info("Current Data Source: {}", dataSourceType.name());
-
-        return dataSourceType;
+        return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
+                ? DataSourceType.READ_DB
+                : DataSourceType.WRITE_DB;
     }
 
 }
