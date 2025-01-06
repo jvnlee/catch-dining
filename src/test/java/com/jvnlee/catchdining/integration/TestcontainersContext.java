57a9e14544c6686d54e6ff3ecf3da93093c9b670
jvnlee <lhjun1028@naver.com>
2025-01-06 16:52:10 +0900

    test: TestcontainersContext를 ComposeContainer 기반으로 변경
    기존처럼 개별 컨테이너들을 직접 올리는 방식은 컨테이너 간 커뮤니케이션을 요구하지 않고, 각 컨테이너가 애플리케이션과만 소통하기 때문에 가능한 구조였음. 그러나 DB를 Master-Slave 구조로 변경함에 따라 2개의 MySQL 컨테이너 사이에도 통신이 필요해졌고, 기왕 하나의 네트워크로 연결시키는 김에 Docker Compose를 활용한 ComposeContainer를 도입함.

--- src/test/java/com/jvnlee/catchdining/integration/TestcontainersContext.java
+++ src/test/java/com/jvnlee/catchdining/integration/TestcontainersContext.java
@@ -2,65 +2,61 @@ package com.jvnlee.catchdining.integration;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.JdbcDatabaseContainer;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.io.File;
 
 public class TestcontainersContext {
 
-    private static final String MYSQL_IMAGE = "mysql:8.0.32";
-    private static final JdbcDatabaseContainer<?> WRITE_DB;
-    private static final JdbcDatabaseContainer<?> READ_DB;
-    private static final String DATABASE_NAME = "catch_dining";
-    private static final String WRITE_DB_INIT_SCRIPT_NAME = "init-write-db.test.sql";
-    private static final String READ_DB_INIT_SCRIPT_NAME = "init-read-db.test.sql";
+    private static final String WRITE_DB = "write-db";
+    private static final String READ_DB = "read-db";
+    private static final int MYSQL_PORT = 3306;
+    private static final String JDBC_URL_FORMAT = "jdbc:mysql://%s:%d/catch_dining";
 
-    private static final String REDIS_IMAGE = "redis:7.0.14";
-    private static final GenericContainer<?> REDIS;
+    private static final String REDIS = "redis";
     private static final int REDIS_PORT = 6379;
 
-    private static final String RABBITMQ_IMAGE = "rabbitmq:3.13.6";
-    private static final RabbitMQContainer RABBITMQ;
+    private static final String RABBITMQ = "rabbitmq";
     private static final int RABBITMQ_PORT = 5672;
 
-    static {
-        WRITE_DB = new MySQLContainer<>(MYSQL_IMAGE)
-                .withDatabaseName(DATABASE_NAME)
-                .withInitScript(WRITE_DB_INIT_SCRIPT_NAME);
-        READ_DB = new MySQLContainer<>(MYSQL_IMAGE)
-                .withDatabaseName(DATABASE_NAME)
-                .withInitScript(READ_DB_INIT_SCRIPT_NAME);
-        REDIS = new GenericContainer<>(REDIS_IMAGE)
-                .withExposedPorts(REDIS_PORT);
-        RABBITMQ = new RabbitMQContainer(RABBITMQ_IMAGE)
-                .withExposedPorts(RABBITMQ_PORT);
+    private static final ComposeContainer COMPOSE_CONTAINER;
+    private static final String COMPOSE_FILE_PATH = "src/test/resources/docker-compose.test.yml";
 
-        WRITE_DB.start();
-        READ_DB.start();
-        REDIS.start();
-        RABBITMQ.start();
+    static {
+        COMPOSE_CONTAINER =
+                new ComposeContainer(new File(COMPOSE_FILE_PATH))
+                        .withExposedService(WRITE_DB, MYSQL_PORT, Wait.forHealthcheck())
+                        .withExposedService(READ_DB, MYSQL_PORT, Wait.forHealthcheck())
+                        .withExposedService(REDIS, REDIS_PORT)
+                        .withExposedService(RABBITMQ, RABBITMQ_PORT);
+
+        COMPOSE_CONTAINER.start();
     }
 
     @DynamicPropertySource
     protected static void dynamicProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", WRITE_DB::getJdbcUrl);
-        registry.add("spring.datasource.username", WRITE_DB::getUsername);
-        registry.add("spring.datasource.password", WRITE_DB::getPassword);
-        registry.add("spring.datasource.driver-class-name", WRITE_DB::getDriverClassName);
+        registry.add("spring.datasource.url", () ->
+                String.format(
+                        JDBC_URL_FORMAT,
+                        COMPOSE_CONTAINER.getServiceHost(WRITE_DB, MYSQL_PORT),
+                        COMPOSE_CONTAINER.getServicePort(WRITE_DB, MYSQL_PORT)
+                )
+        );
 
-        registry.add("spring.datasource.read-db.url", READ_DB::getJdbcUrl);
-        registry.add("spring.datasource.read-db.username", READ_DB::getUsername);
-        registry.add("spring.datasource.read-db.password", READ_DB::getPassword);
-        registry.add("spring.datasource.read-db.driver-class-name", READ_DB::getDriverClassName);
+        registry.add("spring.datasource.read-db.url", () ->
+                String.format(
+                        JDBC_URL_FORMAT,
+                        COMPOSE_CONTAINER.getServiceHost(READ_DB, MYSQL_PORT),
+                        COMPOSE_CONTAINER.getServicePort(READ_DB, MYSQL_PORT)
+                )
+        );
 
-        registry.add("spring.redis.host", REDIS::getHost);
-        registry.add("spring.redis.port", () -> REDIS.getMappedPort(REDIS_PORT).toString());
+        registry.add("spring.redis.host", () -> COMPOSE_CONTAINER.getServiceHost(REDIS, REDIS_PORT));
+        registry.add("spring.redis.port", () -> COMPOSE_CONTAINER.getServicePort(REDIS, REDIS_PORT));
 
-        registry.add("spring.rabbitmq.host", RABBITMQ::getHost);
-        registry.add("spring.rabbitmq.port", () -> RABBITMQ.getMappedPort(RABBITMQ_PORT).toString());
-        registry.add("spring.rabbitmq.username", RABBITMQ::getAdminUsername);
-        registry.add("spring.rabbitmq.password", RABBITMQ::getAdminPassword);
+        registry.add("spring.rabbitmq.host", () -> COMPOSE_CONTAINER.getServiceHost(RABBITMQ, RABBITMQ_PORT));
+        registry.add("spring.rabbitmq.port", () -> COMPOSE_CONTAINER.getServicePort(RABBITMQ, RABBITMQ_PORT));
     }
 
 }
